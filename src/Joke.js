7166a25d405880ff1a652847bc4bb8577820d332
Tracy Jiang <tracyjiangmx@gmail.com>
2021-01-29 16:13:17 -0800

    completed further study

--- src/Joke.js
+++ src/Joke.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import './Joke.css';
-
-/** Render a single joke, along with vote up/down buttons. 
- * props: 
- * - an object like { id, vote, votes, text }
- * state: 
+/** Render a single joke, along with vote up/down buttons.
+ * props:
+ * - an object like { id, vote, votes, text, toggleLock(fn), isLocked(T/F) }
+ * state:
  * -none
  * JokeList -> Joke
 */
 
-function Joke({ id, vote, votes, text, lock}) {
+function Joke({ id, vote, votes, text, toggleLock, isLocked }) {
 	return (
 		<div className="Joke">
 			<div className="Joke-votearea">
 				<button onClick={(evt) => vote(id, +1)}>
 					<i className="fas fa-thumbs-up" />
 				</button>
-
 				<button onClick={(evt) => vote(id, -1)}>
 					<i className="fas fa-thumbs-down" />
 				</button>
-
-				<button onClick={(evt) => lock}>
-				<i className="fas fa-lock"></i>
+				<button onClick={(evt) => toggleLock(id)}>
+					{isLocked ? <i className="fas fa-unlock" /> : <i className="fas fa-lock" />}
 				</button>
-
 				{votes}
 			</div>
-
 			<div className="Joke-text">{text}</div>
 		</div>
 	);
 }
-
 export default Joke;
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Joke from "./Joke";
-import "./JokeList.css";
-import useLocalStorage from "./useLocalStorage";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Joke from './Joke';
+import './JokeList.css';
+import useLocalStorage from './useLocalStorage';
 
 /** List of jokes.
  * 
@@ -11,123 +11,120 @@ import useLocalStorage from "./useLocalStorage";
  * states:
  *  jokes : [joke, ...]
  *  isLoading: T/F
+ *  lockedJokes : [jokes, ...]
  * 
  * App -> JokeList ->Joke
  */
 
 function JokeList({ numJokesToGet }) {
+  //add an isLocked to jokes, centralize
+	const [ jokes, setJokes ] = useLocalStorage('jokes', []);
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ lockedJokes, setLockedJokes ] = useLocalStorage('lockedJokes', []);
 
-  const [jokes, setJokes] = useLocalStorage("jokes", []);
-  const [isLoading, setIsLoading] = useState(true);
-  const [lockedJokes, setLockedJokes] = useLocalStorage("lockedJokes", []);
+	useEffect(
+		function getJokes() {
+			async function getJokesFromApi() {
+				if (jokes.length === 0) {
+					try {
+            // load jokes one at a time, adding not-yet-seen jokes
+            // be cautious with naming, jokes already in state. shadowing
+            //pick a different name for line 32 jokes
+            //filter out locked jokes from jokes state instead of keeping another state lockedJoke
+						let jokes = [ ...lockedJokes ];
+						let seenJokes = new Set();
 
-  useEffect(function getJokes() {
-    async function getJokesFromApi() {
-      console.log("jokes on mount", jokes);
-      if (jokes.length === 0) {
+						while (jokes.length < numJokesToGet) {
+							let res = await axios.get('https://icanhazdadjoke.com', {
+								headers: { Accept: 'application/json' }
+							});
+							let { ...joke } = res.data;
+              
+							if (!seenJokes.has(joke.id)) {
+								seenJokes.add(joke.id);
+								jokes.push({ ...joke, votes: 0 });
+							} else {
+								console.log('duplicate found!');
+							}
+						}
+						setJokes(jokes);
+					} catch (err) {
+						console.error(err);
+					}
+				}
+				setIsLoading(false);
+			}
+			if (isLoading === true) {
+				getJokesFromApi();
+			}
+		},
+		[ isLoading, numJokesToGet, setJokes, jokes.length, lockedJokes ]
+	);
 
-        try {
-          // load jokes one at a time, adding not-yet-seen jokes
-          let jokes = [];
-          let seenJokes = new Set();
+	/* Helper function to generate new jokes */
+	function generateNewJokes() {
+		setJokes([]);
+		setIsLoading(true);
+	}
 
-          while (jokes.length + lockedJokes.length < numJokesToGet) {
-            let res = await axios.get("https://icanhazdadjoke.com", {
-              headers: { Accept: "application/json" }
-            });
-            let { ...joke } = res.data;
+	/* handle reset button click, sets all votes to 0 */
+	function resetVotes() {
+    const resetedJokes = jokes.map((j) => ({ ...j, votes: 0 }));
+    //should use callback, depending on previous state
+		setJokes(resetedJokes);
+	}
 
-            if (!seenJokes.has(joke.id)) {
-              seenJokes.add(joke.id);
-              jokes.push({ ...joke, votes: 0 });
-            } else {
-              console.log("duplicate found!");
-            }
-          }
-          jokes = jokes.concat(lockedJokes);
-          console.log("jokes on mount", jokes);
-          setJokes("jokes", jokes);
+	/* Toggle lock/unlock for a joke and update the lockedJoke array  */
+	function toggleLockJoke(id) {
+		if (lockedJokes.find((j) => j.id === id)) {
+			setLockedJokes((lockedJokes) => lockedJokes.filter((j) => j.id !== id));
+		} else {
+			const lockedJoke = jokes.find((j) => j.id === id);
+			setLockedJokes((lockedJokes) => [ ...lockedJokes, lockedJoke ]);
+		}
+	}
+	/* change vote for this id by delta (+1 or -1) */
+	function vote(id, delta) {
+		const updatedJokes = jokes.map((j) => (j.id === id ? { ...j, votes: j.votes + delta } : j));
+		const sortedJokes = updatedJokes.sort((a, b) => b.votes - a.votes);
+		setJokes(sortedJokes);
+	}
 
-        } catch (err) {
-          console.error(err);
-        }
-      }
-      setIsLoading(false);
-    };
-    if (isLoading === true) {
-      getJokesFromApi();
-    }
-  }, [isLoading, numJokesToGet, setJokes, jokes.length]);
+	if (isLoading) {
+		return (
+			<div className="loading">
+				<i className="fas fa-4x fa-spinner fa-spin" />
+			</div>
+		);
+	}
 
+	return (
+		<div className="JokeList">
+			<button className="JokeList-getmore" onClick={generateNewJokes}>
+				Get New Jokes
+			</button>
 
-  /* Helper function to generate new jokes */
-  function generateNewJokes() {
-    setJokes("jokes", []);
-    setIsLoading(true);
-  }
+			<button className="JokeList-reset" onClick={resetVotes}>
+				reset the votes
+			</button>
 
-  /* handle reset button click, sets all votes to 0 */
-  function resetVotes() {
-    const resetedJokes = jokes.map(j => ({ ...j, votes: 0 }));
-    setJokes("jokes",resetedJokes);
-  }
-
-  /* lock a joke and update the array  */
-  function lockJoke(id) {
-    const lockedJoke = jokes.find(j => j.id === id);
-    setLockedJokes("lockedJokes", lockedJokes => [...lockedJokes, lockedJoke])
-  }
-  /* change vote for this id by delta (+1 or -1) */
-  function vote(id, delta) {
-    const updatedJokes = jokes.map(j =>
-      j.id === id ? { ...j, votes: j.votes + delta } : j)
-    const sortedJokes = updatedJokes.sort((a, b) => b.votes - a.votes);
-    setJokes("jokes", sortedJokes);
-  }
-
-  if (isLoading) {
-    return (
-      <div className="loading">
-        <i className="fas fa-4x fa-spinner fa-spin" />
-      </div>
-    )
-  }
-
-  return (
-
-    <div className="JokeList">
-      <button
-        className="JokeList-getmore"
-        onClick={generateNewJokes}
-      >
-        Get New Jokes
-    </button>
-
-      <button
-        className="JokeList-reset"
-        onClick={resetVotes}
-      >
-        reset the votes
-    </button>
-
-      {jokes.map(j => (
-        <Joke
-          text={j.joke}
-          key={j.id}
-          id={j.id}
-          votes={j.votes}
-          vote={vote}
-          lock={lockJoke}
-          isLocked={lockedJokes.some(l => l.id === j.id)}
-        />
-      ))}
-    </div>
-
-  )
+			{jokes.map((j) => (
+				<Joke
+					text={j.joke}
+					key={j.id}
+					id={j.id}
+					votes={j.votes}
+					vote={vote}
+					toggleLock={toggleLockJoke}
+					isLocked={lockedJokes.some((l) => l.id === j.id)}
+				/>
+			))}
+		</div>
+	);
 }
 
 JokeList.defaultProps = {
-  numJokesToGet: 5
-}
+	numJokesToGet: 5
+};
 
 export default JokeList;
